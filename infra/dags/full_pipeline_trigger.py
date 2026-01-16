4c6df92021ca03a73539d042c6fd92d0c87e76d6
Lucas H <hosoyalucas@gmail.com>
2026-01-16 08:03:17 -0300

    Full pipe

---
+++ infra/dags/full_pipeline_trigger.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+
+
+with DAG(
+    dag_id='kafka_to_lakehouse_full_pipeline_trigger',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None, 
+    catchup=False
+) as dag:
+
+    kafka = TriggerDagRunOperator(
+        task_id='kafka',
+        trigger_dag_id='producer_dag',
+        conf={"event_no": "1"},
+            
+    )
+    
+    kafka_to_bronze = TriggerDagRunOperator(
+        task_id='kafka_to_bronze',
+        trigger_dag_id='lakehouse_kafka_to_bronze',
+        conf={"table_path": "s3a://lakehouse/bronze/final_events"},
+        poke_interval=5,
+    )
+    
+    bronze_to_silver = TriggerDagRunOperator(
+        task_id='bronze_to_silver',
+        trigger_dag_id='lakehouse_bronze_to_silver',
+        conf={"bronze_path": "s3a://lakehouse/bronze/final_events", "silver_path": "s3a://lakehouse/silver/final_events"},
+        poke_interval=5,
+    )
+    
+    kafka >> kafka_to_bronze >> bronze_to_silver
